1fe5d8c17a5bbb80fc31fae79d41cb9517552737
Kuzma Terentyev <terentyev.kuzma@gmail.com>
2024-06-13 20:09:30 +0600

    lesson2_task2 added

---
+++ lesson2/task2/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = 10;
+int[] array = new int[n];
+int i = 0;
+
+while (i < array.Length)           // while (i < n)
+{
+    array[i] = i + 1;
+    //Console.Write(array[i]);
+    //Console.Write(' ');
+    Console.Write($"{array[i]} ");
+    i = i + 1;
+}
