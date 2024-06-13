8442ed44ff3b2ea5faaed0a2570ac4d3db917967
Kuzma Terentyev <terentyev.kuzma@gmail.com>
2024-06-13 18:42:54 +0600

    lesson2:task1 added

---
+++ lesson2/task1/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] array = new int[5];
+array[0] = 5;
+array[1] = 3;
+array[2] = 10;
+array[3] = 7;
+array[4] = 9;
+Console.WriteLine(array[0]);
+Console.WriteLine(array[1]);
+Console.WriteLine(array[2]);
+Console.WriteLine(array[3]);
+Console.WriteLine(array[4]);
+
+// Console.WriteLine(array[5]);
+
+int[] array2 = new int[5] { 1, 2, 3, 4, 5 };
+Console.WriteLine(array2[0]);
+
+int[] array3 = { 1, 2, 3, 4, 5 };
+
