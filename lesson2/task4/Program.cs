2d187bec8c138531af803747b41aef48fc8327f1
Kuzma Terentyev <terentyev.kuzma@gmail.com>
2024-06-13 21:13:46 +0600

    lesson2:task4 added

---
+++ lesson2/task4/Program.cs
@@ -0,0 +1,33 @@
+﻿int n = 10;
+int[] array = { 1, 2, 3, 4, 15, 6, 7, 8, 9, 10 };
+int i = 0;
+int max = array[0];
+while (i < n)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    i = i + 1;
+}
+Console.WriteLine(max);
+
+max = array[0];
+for (int j = 0; j < n; j++)
+{
+    if (array[j] > max)
+    {
+        max = array[j];
+    }
+}
+Console.WriteLine(max);
+
+max = array[0];
+foreach (int e in array)
+{
+    if (e > max)
+    {
+        max = e;
+    }
+}
+Console.WriteLine(max);
