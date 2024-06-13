6864702b92246a5afb46895444cf2a08b9c0faca
Kuzma Terentyev <terentyev.kuzma@gmail.com>
2024-06-13 18:22:58 +0600

    lesson1:task1 added

---
+++ lesson1/task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+// Console.WriteLine("Hello world!");
+
+// типы данных int, long - целые числа
+// float, double - вещественные (дробные)
+// char - символьный
+// bool - логический
+
+int value = 100;
+Console.WriteLine(value);
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
