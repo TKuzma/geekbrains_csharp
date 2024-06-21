79a3eda0d7367619f63d0046d688b4137e0b64f3
Kuzma Terentyev <terentyev.kuzma@gmail.com>
2024-06-21 19:17:45 +0600

    homework1:task4 added

---
+++ homework1/task4/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, 
+// которая на вход принимает число (number), 
+// а на выходе выводит все чётные числа от 1 до number (включительно), 
+// после каждого числа должен быть знак пробела.
+
+System.Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+// int i = 1;                         // пример решения с циклом while
+
+// while (i <= number)
+// {
+//     if (i % 2 == 0)
+//     {
+//         System.Console.Write(i + " ");
+//     }
+//     i++;
+// }
+
+
+
+for (int i = 1; i <= number; i++)       // пример решения с циклом for
+{
+    if (i % 2 == 0)
+    {
+        System.Console.Write(i + " ");
+    }
+}
