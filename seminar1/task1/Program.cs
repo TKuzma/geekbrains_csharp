7d4636fa9f9306cb7d8ca24d1fc664a092e503ac
Kuzma Terentyev <terentyev.kuzma@gmail.com>
2024-06-21 15:44:32 +0600

    seminar1:task1 added

---
+++ seminar1/task1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая на вход
+// принимает два целых числа и проверяет,
+// является ли первое число квадратом второго.
+// a = 25, b = 5 => да
+// a = 2, b = 10 => нет
+// a = 9, b = -3 => да
+// a = -3, b = 9 => нет
+
+// int firstNumber = 9;
+// int secondNumber = 4;
+
+System.Console.WriteLine("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber == secondNumber * secondNumber)
+{
+    Console.WriteLine("a = " + firstNumber + ", b = " + secondNumber + " => да");
+}
+else
+{
+    Console.WriteLine($"a = {firstNumber}, b = {secondNumber} => нет");
+}
+
+
+// System.Console.WriteLine("Введите первое число: ");
+
+// string strNumber = Console.ReadLine();
+// int firstNumber = int.Parse(strNumber);        
+
+// int firstNumber = int.Parse(Console.ReadLine();)  вместо int.Parse можно использовать Convert.ToInt32
+// самому попробовать сделать с ридлайн
+
+// ToInt16 - конвертация в byte
+// ToInt32 - конвертация в int
+// ToInt64 - конвертация в long
