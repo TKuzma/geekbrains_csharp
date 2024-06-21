7c4a69e324e2de473d19b11eca6026dbb789e9e7
Kuzma Terentyev <terentyev.kuzma@gmail.com>
2024-06-21 17:49:10 +0600

    homework1:task1 done added

--- homework1/task1/Program.cs
+++ homework1/task1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, 
+// которая на вход принимает два числа и выводит, 
+// какое число большее, а какое меньшее.
+
+System.Console.WriteLine("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber > secondNumber || firstNumber < secondNumber)
+    if (firstNumber > secondNumber)
+    {
+        System.Console.WriteLine($"Первое число больше: {firstNumber} > {secondNumber}");
+    }
+    else 
+    {
+        System.Console.WriteLine($"Второе число больше: {secondNumber} > {firstNumber}");
+    }
+else
+{
+    System.Console.WriteLine("Они равны!");
+}
