52d917db0835384c7ada30ad3c364fceea0437b5
Kuzma Terentyev <terentyev.kuzma@gmail.com>
2024-06-21 17:17:24 +0600

    seminar1:task3 added

---
+++ seminar1/task3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход
+// трёхзначное целое число и на выходе показывает сумму
+// первой и последней цифры этого числа.
+// 456 => 10
+// 782 => 9
+// 918 => 17
+
+System.Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 && number < 1000)
+{
+    int firstDigit = number / 100;
+    int thirdDigit = number % 10;
+    int sum = firstDigit + thirdDigit;
+    System.Console.WriteLine(sum);
+    //System.Console.WriteLine(number / 100 + number % 10);
+}
+else
+{
+    System.Console.WriteLine("Некорректное число!");
+}
