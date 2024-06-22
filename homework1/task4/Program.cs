99fe94451e96ee6dd34b3ed42b7072835668e416
Kuzma Terentyev <terentyev.kuzma@gmail.com>
2024-06-22 15:29:06 +0600

    more ex task4:homework1

--- homework1/task4/Program.cs
+++ homework1/task4/Program.cs
@@ -3,8 +3,8 @@
 // а на выходе выводит все чётные числа от 1 до number (включительно), 
 // после каждого числа должен быть знак пробела.
 
-System.Console.WriteLine("Введите число");
-int number = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите число");
+// int number = Convert.ToInt32(Console.ReadLine());
 
 
 // int i = 1;                         // пример решения с циклом while
@@ -20,10 +20,51 @@ int number = Convert.ToInt32(Console.ReadLine());
 
 
 
-for (int i = 1; i <= number; i++)       // пример решения с циклом for
-{
-    if (i % 2 == 0)
-    {
-        System.Console.Write(i + " ");
-    }
-}
+// for (int i = 1; i <= number; i++)       // пример решения с циклом for
+// {
+//     if (i % 2 == 0)
+//     {
+//         System.Console.Write(i + " ");
+//     }
+// }
+
+
+
+// using System;
+
+// public class Answer
+// {
+//     static void PrintEvenNumbers(int number)
+//     {
+//         // Введите свое решение ниже
+//         for (int i = 1; i <= number; i++)       // пример решения с циклом for
+//         {
+//             if (i % 2 == 0)
+//             {
+//                 System.Console.Write(i + " ");
+//             }
+//         }
+
+
+//     }
+
+
+//     // Не удаляйте и не меняйте метод Main! 
+//     static public void Main(string[] args)
+//     {
+//         int number;
+
+//         if (args.Length >= 1)
+//         {
+//             number = int.Parse(args[0]);
+//         }
+//         else
+//         {
+//             // Здесь вы можете поменять значения для отправки кода на Выполнение
+//             number = 9;
+//         }
+
+//         // Не удаляйте строки ниже
+//         PrintEvenNumbers(number);
+//     }
+// }
