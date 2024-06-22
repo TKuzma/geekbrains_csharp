e9b1a215fb710272fec6c8856bcc295aa05df3c4
Kuzma Terentyev <terentyev.kuzma@gmail.com>
2024-06-22 15:20:28 +0600

    more ex in hw1:task3

--- homework1/task3/Program.cs
+++ homework1/task3/Program.cs
@@ -2,15 +2,56 @@
 // которая на вход принимает число number и выводит, 
 // является ли число чётным (делится ли оно на два без остатка).
 
-System.Console.WriteLine("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
 
-if (number % 2 == 0)
-{
-    System.Console.WriteLine($"Число {number} является четным");
-}
-else
-{
-    System.Console.WriteLine($"Число {number} является нечетным");
-}
+// if (number % 2 == 0)
+// {
+//     System.Console.WriteLine($"Число {number} является четным");
+// }
+// else
+// {
+//     System.Console.WriteLine($"Число {number} является нечетным");
+// }
+
+
+
+// using System;
+
+// class Answer
+// {
+//     static void CheckIfEven(int number)
+//     {
+//         if (number % 2 == 0)
+//         {
+//             System.Console.WriteLine("четное");
+//         }
+//         else
+//         {
+//             System.Console.WriteLine("нечетное");
+//         }
+
+
+//     }
+
+
+//     // Не удаляйте и не меняйте метод Main! 
+//     static public void Main(string[] args)
+//     {
+//         int number;
+
+//         if (args.Length >= 1)
+//         {
+//             number = int.Parse(args[0]);
+//         }
+//         else
+//         {
+//             // Здесь вы можете поменять значения для отправки кода на Выполнение
+//             number = 7;
+//         }
+
+//         // Не удаляйте строки ниже
+//         CheckIfEven(number);
+//     }
+// }
 
