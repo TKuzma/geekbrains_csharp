08e2cb3c280fe32f3d9537d42c58ab08cfe7f191
Kuzma Terentyev <terentyev.kuzma@gmail.com>
2024-06-25 18:35:31 +0600

    seminat2: task1 + task2 added

---
+++ seminar2/task1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на
+// вход трёхзначное число и удаляет вторую
+// цифру этого числа.
+// Примеры
+// a = 256 => 26
+// a = 891 => 81
+
+
+System.Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 && number < 1000)
+{
+    // System.Console.Write(number / 100 + "" + number % 10);
+    int result = Convert.ToInt32(number / 100 + "" + number % 10);
+    // int result = number / 100 * 10 + number % 10;
+    System.Console.WriteLine(result);
+}
+else
+{
+    System.Console.WriteLine("Вы ввели некорректное число!");
+}
+
+
---
+++ seminar2/task2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход
+// трёхзначное число и возводит вторую цифру этого
+// числа в степень, равную третьей цифре.
+
+
+System.Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 && number < 1000)
+{
+    int secondDigit = number / 10 % 10;   // Узнали вторую цифру
+    int result = 1;
+    for (int power = number % 10; power > 0; power--)
+    {
+        result *= secondDigit;
+    }
+    System.Console.WriteLine(result);
+}
+else
+{
+    System.Console.WriteLine("Вы ввели некорректное число!");
+}
