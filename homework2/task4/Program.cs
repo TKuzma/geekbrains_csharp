e0094f8808aa2d113b926a572eb61f9886d77247
Kuzma Terentyev <terentyev.kuzma@gmail.com>
2024-06-30 19:19:08 +0600

    homework2 done

---
+++ homework2/task4/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 4: Напишите программу, 
+// которая на вход принимает натуральное число N, 
+// а на выходе показывает его цифры через запятую.
+
+System.Console.WriteLine("Введите натуральное число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 0)
+{
+    if (number < 10)
+    {
+        Console.WriteLine(number);
+    }
+    else
+    {
+        while (number > 0)
+        {
+            int digit = number % 10;
+            number /= 10;
+            if (number > 0)
+            {
+                Console.Write(digit + ",");
+            }
+            else
+            {
+                Console.WriteLine(digit);
+            }
+        }
+    }   
+}    
+else 
+{
+    System.Console.WriteLine("Ошибка! Введите натуралльное число");
+}
