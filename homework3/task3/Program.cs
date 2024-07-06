2f12c8a9a17749544ff23ca32c73bb46ff27ce1b
Kuzma Terentyev <terentyev.kuzma@gmail.com>
2024-07-06 20:02:07 +0600

    homework3:task4 added

---
+++ homework3/task3/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 3: 
+// Задайте массив из вещественных чисел с
+// ненулевой дробной частью. Найдите разницу между
+// максимальным и минимальным элементов массива.
+
+
+
+// Написать описание
+Random rand = new Random();
+
+double[] array = new double[10];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = Math.Round(rand.Next(0, 10) + rand.NextDouble(), 2);
+    System.Console.Write(array[i] + " ");
+}
+System.Console.WriteLine();
+
+double maxValue = 0;
+double minValue = 5.5;
+
+for (int i = 0; i < array.Length; i++)  // Обязательно на семинар ВОПРОС ПО ДЗ
+{
+    if (minValue > array[i])
+    {
+        minValue = array[i];
+    }
+    else if (maxValue < array[i])
+    {
+        maxValue = array[i];
+    }
+}
+System.Console.Write("Минимальное значение: " + minValue + " Максимальное значение: " + maxValue);
+System.Console.WriteLine("\nРазница: " + (maxValue - minValue));
+
+
---
+++ homework3/task4/Program.cs
@@ -0,0 +1,77 @@
+﻿// Задача 4**(не обязательно): 
+// Дано натуральное
+// число в диапазоне от 1 до 100 000. Создайте массив,
+// состоящий из цифр этого числа. Старший разряд
+// числа должен располагаться на 0-м индексе
+// массива, младший – на последнем. Размер массива
+// должен быть равен количеству цифр.
+
+
+// Попробуй перевернуть массив мы такое делали, а вот как записать все эти числа в массив сам думай, но, наверное с циклом
+
+// Ввод данных
+System.Console.WriteLine("Введите натуральное число от 1 до 100 000: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// Создание временного массива    
+int[] array = new int[6];
+int count = 0;
+
+// Проверка правильности вводимых данных
+if (number >= 1 && number <= 100000)
+{
+
+
+    // Извлеченеие цифр и запись их в массив
+    while (number > 0)
+    {
+        array[count] = number % 10;
+        number /= 10;
+        count++;
+    }
+}
+// При неправильном вводе данных сообщение об ошибке
+else
+{
+    System.Console.WriteLine("Ошибка! Введите натуралльное число от 1 до 100 000");
+}
+
+// Создание массива нужного размера
+int[] result = new int[count];
+Array.Copy(array, result, count);
+
+// // Вывод данных в терминал
+// for (int i = 0; i < result.Length; i++)
+// {
+//     if (i < result.Length - 1)
+//         System.Console.Write(result[i] + ", ");
+//     else 
+//         System.Console.Write(result[i]);
+// }
+
+ 
+// Переворот массива в нужном порядке
+int i = 0;
+int i2 = result.Length - 1;
+while (i <= result.Length / 2)
+{
+    int temp = result[i];
+    result[i] = result[i2];
+    result[i2] = temp;
+    i++;
+    i2 = i2 - 1;
+}
+
+// Вывод массива в терминал
+System.Console.WriteLine();
+for (int j = 0; j < result.Length; j++)
+{
+    if (j < result.Length - 1)
+        System.Console.Write(result[j] + ", ");
+    else
+        System.Console.Write(result[j]);
+}
+
+
+
+
