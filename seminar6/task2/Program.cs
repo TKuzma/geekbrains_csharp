baa1adfe0693a88f9e79162050a7adc529ad4d9c
Kuzma Terentyev <terentyev.kuzma@gmail.com>
2024-07-28 19:09:01 +0600

    seminar5: task1-2-3 added

---
+++ seminar6/task2/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задание 2.
+// На основе символов строки (тип string) сформировать массив
+// символов (тип char[]). Вывести массив на экран.
+// Указание
+// Метод строки ToCharArray() не использовать.
+// Пример
+// “Hello!” => [‘H’, ‘e’, ‘l’, ‘l’, ‘o’, ‘!’ ] 
+
+void Main()
+{
+    string? str = Console.ReadLine();
+
+    // Первый способ
+    char[] chars = str!.ToCharArray();
+    PrintCharArray(chars);
+    System.Console.WriteLine();
+
+    // Второй способ
+    char[] chars2 = StringToCharArray(str);
+    PrintCharArray(chars2);
+}
+
+char [] StringToCharArray(string? str)
+{
+    char[] chars = new char[str!.Length];
+
+    for (int i = 0; i < str.Length; i++)
+        chars[i] = str[i];
+    return chars;
+}
+
+void PrintCharArray(char[] chars)
+{
+    System.Console.Write(string.Join(", ", chars));
+}
+
+Main();
---
+++ seminar6/task3/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задание 3.
+// Считать строку с консоли, состоящую из латинских
+// букв в нижнем регистре. Выяснить, сколько среди
+// введённых букв гласных.
+// Пример
+// “hello” => 2
+// “world” => 1
+
+void Main()
+{
+    string? str = Console.ReadLine();
+    str = str!.ToLower();
+    System.Console.Write(CountVowels(str!));
+}
+
+int CountVowels(string? str)
+{
+    int count = 0;
+
+    foreach(char c in str!)
+        if (IsVowel(c))
+            count++;
+    
+    return count;
+}
+
+bool IsVowel(char c)
+{
+    return "aeouyi".Contains(c);
+}
+
+Main();
