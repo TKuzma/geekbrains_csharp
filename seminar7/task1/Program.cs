fae20acc516c1e8b78c8133f3d3e30b4cbd9f5a2
Kuzma Terentyev <terentyev.kuzma@gmail.com>
2024-07-29 13:21:44 +0600

    seminar7:task1-2-3 added

---
+++ seminar7/task1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задание 2.
+// Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N.
+// Указание
+// Использовать рекурсию. Не использовать цикл.
+// Пример
+// N=5 => 1 2 3 4 5
+
+void Main()
+{
+    System.Console.Write("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    PrintNumber(number);
+}
+
+void PrintNumber(int n)
+{
+    if (n < 1) return;
+    PrintNumber(n - 1);
+    System.Console.Write(n + " ");
+}
+
+Main();
---
+++ seminar7/task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задание 2.
+// Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N.
+// Указание
+// Использовать рекурсию. Не использовать цикл.
+// Пример
+// N=5 => 1 2 3 4 5
+
+void Main()
+{
+    System.Console.Write("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    System.Console.Write(SumDigitsOfNumber(number));  
+}
+
+int SumDigitsOfNumber(int N)
+{
+    if(N < 1) return 0;
+    return N % 10 + SumDigitsOfNumber(N / 10);
+}
+
+Main();
